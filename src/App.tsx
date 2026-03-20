cc7f4e6c6a4cb15a694c3a7c4cd42557654a28a9
Benjamin Krug <benjaminkrug@yahoo.com>
2026-03-20 12:59:34 -0400

    Logo changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
+import breakdancingSquirrel from "./breakdancing-squirrel.jpg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={breakdancingSquirrel} className="App-logo" alt="breakdancing squirrel" />
+        <h2>Great Dancing Squirrel Corporation of North America</h2>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          In our world, the entertainment industry is now nearly monopolized by squirrel performers.
+          A major subindustry involves dancing, and GDSC has decades of experience training and coaching squirrels to maximize their talents.
+          Signup with us today to prepare your squirrel for the next great American musical or to awe a stadium full of fans.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
