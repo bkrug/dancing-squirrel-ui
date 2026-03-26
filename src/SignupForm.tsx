a6baa67cb25e4bfa2fc9fa4ab0c0f2a206cf7e3b
Benjamin Krug <benjaminkrug@yahoo.com>
2026-03-26 14:12:47 -0400

    Deserialize validation failures according to standards

--- src/SignupForm.tsx
+++ src/SignupForm.tsx
@@ -6,8 +6,20 @@ class TrainingRequestValidationFailures {
   caretakerType: string = "";
   caretakerName: string = "";
   email: string = "";
-  phone: string = "not a valid phone number";
+  phone: string = "";
   squirrelName: string = "";
+}
+
+function toCamelCase(key: string, value: any) {
+  if (value && typeof value === 'object'){
+    for (var k in value) {
+      if (/^[A-Z]/.test(k) && Object.hasOwnProperty.call(value, k)) {
+        value[k.charAt(0).toLowerCase() + k.substring(1)] = value[k];
+        delete value[k];
+      }
+    }
+  }
+  return value;
 }
 
 export default function useTrainingRequestForm() {
@@ -19,19 +31,25 @@ export default function useTrainingRequestForm() {
       method: "POST",
       body: formData
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log("Record saved", data);
+    .then(response => response.status >= 400 ? response.text() : null )
+    .then(jsonString => {
+      if (!jsonString) return;
+      //TODO: Use generics here
+      let failures = Object.assign(new TrainingRequestValidationFailures(), JSON.parse(jsonString, toCamelCase));
+      console.log('validation failure', failures)
+      setErrors(failures);
     })
-    .catch(errors => {
-      console.log(errors);
-      setErrors(errors);
+    .catch(httpErrors => {
+      console.log(httpErrors);
+      setErrors(httpErrors);
     });
   }
 
+  console.log('errors from state', errors);  
+
   return (
     <form action={postRequest} method="POST">
-      <div>
+      <div className="field">
         <label htmlFor="caretakertype">Caretaker Type</label>
         <div className="radiogroup">
           <input type="radio" name="caretakertype" id="company" value="company" defaultChecked />
@@ -41,26 +59,26 @@ export default function useTrainingRequestForm() {
         </div>
         {errors.caretakerType && <div className='error'>{errors.caretakerType}</div>}
       </div>
-      <p>
+      <div className="field">
         <label htmlFor="caretakername">Caretaker Name</label>
         <input type="text" name="caretakername" required />
         {errors.caretakerName && <div className='error'>{errors.caretakerName}</div>}
-      </p>
-      <p>
+      </div>
+      <div className="field">
         <label htmlFor="email">Email</label>
         <input type="email" name="email" required />
         {errors.email && <div className='error'>{errors.email}</div>}
-      </p>
-      <p>
+      </div>
+      <div className="field">
         <label htmlFor="phone">Phone</label>
         <input type="tel" name="phone" />
         {errors.phone && <div className='error'>{errors.phone}</div>}
-      </p>
-      <p>
+      </div>
+      <div className="field">
         <label htmlFor="squirrelname">Squirrel Name</label>
         <input type="text" name="squirrelname" required />
         {errors.squirrelName && <div className='error'>{errors.squirrelName}</div>}
-      </p>
+      </div>
       <button type="submit">Register Squirrel</button>
     </form>
   );
