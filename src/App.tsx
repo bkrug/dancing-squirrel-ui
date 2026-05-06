16cb218f7aea3935b756b555ce73395fa007f139
Benjamin Krug <benjaminkrug@yahoo.com>
2026-05-06 02:32:50 -0400

    Change page header from customer portal

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={`${process.env.PUBLIC_URL}/breakdancing-squirrel.jpg`} className="App-logo" alt="breakdancing squirrel" />
-        <h2>Great Dancing Squirrel Corporation of North America</h2>
-        <p>
-          In our world, the entertainment industry is now nearly monopolized by squirrel performers.
-          A major subindustry involves dancing, and GDSC has decades of experience training and coaching squirrels to maximize their talents.
-          Signup with us today to prepare your squirrel for the next great American musical or to awe a stadium full of fans.
-        </p>
+        <div>
+          <h2>Great Dancing Squirrel Corporation of North America</h2>
+          <p>In our world, the entertainment industry is now nearly monopolized by squirrel performers.</p>
+          <p>A major subindustry involves dancing, and GDSC has decades of experience training and coaching squirrels to maximize their talents.</p>
+          <p>Signup with us today to prepare your squirrel for the next great American musical or to awe a stadium full of fans.</p>
+          <p><a href="/employee">Switch to Employee Portal</a></p>
+        </div>
       </header>
       <CustomerSignup />
     </div>
