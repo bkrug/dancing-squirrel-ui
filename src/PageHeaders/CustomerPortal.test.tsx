6045da0fc38a8e172d7c5ac845e49bba289f6a32
Benjamin Krug <benjaminkrug@yahoo.com>
2026-05-08 14:52:17 -0400

    Have a passing test

--- src/PageHeaders/CustomerPortal.test.tsx
+++ src/PageHeaders/CustomerPortal.test.tsx
@@ -2,7 +2,10 @@ import { render, screen } from '@testing-library/react';
 import CustomerPortal from './CustomerPortal';
 
 test('renders learn react link', () => {
-  render(<CustomerPortal child={<h2>Hello, World!</h2>}/>);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<CustomerPortal child={<h2 className='expect-in-test'>Child Element used for Unit Test</h2>}/>);
+
+  //Asert that child element was rendered
+  const linkElement = screen.getByText(/Child Element used for Unit Test/i);
   expect(linkElement).toBeInTheDocument();
+  expect(linkElement.className).toEqual('expect-in-test');
 });
