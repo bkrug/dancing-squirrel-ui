9227df6bcd3d3c4fea00a34e05e362c216094e87
Benjamin Krug <benjaminkrug@yahoo.com>
2026-05-09 21:59:25 -0400

    Add test

--- src/Pages/OnboardCustomer/OnboardCustomer.test.tsx
+++ src/Pages/OnboardCustomer/OnboardCustomer.test.tsx
@@ -4,6 +4,7 @@ import { act } from 'react';
 import TrainingRequest from '../../DbModels/TrainingRequest';
 import { CaretakerType } from '../../enums';
 import { getParsedResponse } from '../../Forms/Submission/formikSubmission';
+import { GenericModelResponse } from '../../Forms/Submission/formResponse';
 import { getSiblingByText } from '../../testhelpers';
 import OnboardCustomer from './OnboardCustomer';
 
@@ -134,4 +135,71 @@ test('Given a training request id that exists, with a person as a caretaker, and
   expect(getSiblingByText(/Description of Needs/i)?.textContent).toEqual('This squirrel has potential to exploit');
   expect(getSiblingByText(/Employee who did Onboarding/i)?.textContent).toEqual('someUser');
   expect(getSiblingByText(/Date of Onboarding/i)?.textContent).toEqual('1970-01-01T01:02:00.000Z');
+});
+
+test('Given a training request with a phone number that is missing the area code. Expect the phone number to be rendered correctly, but omitting the area code.', async () => {
+  //Arrange
+  const mockedUseNavigate = jest.fn();
+  jest.mock('react-router', () => ({
+    ...jest.requireActual('react-router-dom'), // use actual for all non-hook parts
+    useNavigate: () => mockedUseNavigate,
+    useParams: () => ({ trainingRequestId: '5' }),
+  }));
+
+  const recFromDb: TrainingRequest = {
+    trainingRequestId: 5,
+    squirrelName: 'Mittens',
+    caretakerType: CaretakerType.Person,
+    organizationName: 'This value would be null in real life. But even if it is non-null, we should not render the company name.',
+    ownerLastName: 'Robinson',
+    ownerFirstName: 'Mrs.',
+    email: 'song@beatles.com',
+    phone: '4145552222',
+    squirrelId: null,
+    onboardUsername: null,
+    onboardingDateTimeUnix: null,
+    descriptionOfNeeds: 'This squirrel has potential to exploit'
+  };
+  const responseFromHttpGetRequest = Promise.resolve(Effect.succeed(recFromDb));
+  (getParsedResponse as jest.Mock).mockReturnValueOnce(responseFromHttpGetRequest);
+
+  //Act
+  await act(async () => render(<OnboardCustomer />));
+
+  //Asert that child element was rendered
+  expect(getSiblingByText(/Phone/i)?.textContent).toEqual('(414) 555-2222');
+});
+
+test('Given a training request id that does not exist. Expect a failure message to be displayed to the user.', async () => {
+  //Arrange
+  const mockedUseNavigate = jest.fn();
+  jest.mock('react-router', () => ({
+    ...jest.requireActual('react-router-dom'), // use actual for all non-hook parts
+    useNavigate: () => mockedUseNavigate,
+    useParams: () => ({ trainingRequestId: '1001' }),
+  }));
+
+  const failureObj : GenericModelResponse<string> = {
+    isSuccess: false,
+    isInternalError: false,
+    validationFailures: 'Not Found'
+  }
+  const responseFromHttpGetRequest = Promise.resolve(Effect.fail(failureObj));
+  (getParsedResponse as jest.Mock).mockReturnValueOnce(responseFromHttpGetRequest);
+
+  //Act
+  await act(async () => render(<OnboardCustomer />));
+
+  //Asert that child element was rendered
+  expect(screen.queryByText(/Not Found/i)).toBeInTheDocument();
+
+  expect(screen.queryByText(/Is Onboarded/i)).not.toBeInTheDocument();
+  expect(screen.queryByText(/Squrriel Name/i)).not.toBeInTheDocument();
+  expect(screen.queryByText(/Caretaker Type/i)).not.toBeInTheDocument();
+  expect(screen.queryByText(/Caretaker Name/i)).not.toBeInTheDocument();
+  expect(screen.queryByText(/Email/i)).not.toBeInTheDocument();
+  expect(screen.queryByText(/Phone/i)).not.toBeInTheDocument();
+  expect(screen.queryByText(/Description of Needs/i)).not.toBeInTheDocument();
+  expect(screen.queryByText(/Employee who did Onboarding/i)).not.toBeInTheDocument();
+  expect(screen.queryByText(/Date of Onboarding/i)).not.toBeInTheDocument();
 });
