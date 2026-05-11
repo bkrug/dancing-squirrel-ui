0c003a4cba2dda185be7e2573f4ac8f0212ef49e
Benjamin Krug <benjaminkrug@yahoo.com>
2026-05-11 02:55:58 -0400

    Add parsing tests

--- src/Forms/Submission/jsonParsing.test.ts
+++ src/Forms/Submission/jsonParsing.test.ts
@@ -10,11 +10,27 @@ class ObjectInFrontendFormat {
   valueB: number = 0;
 }
 
-test('', () => {
+test('Expect JSON object that has capital letters at beginning of property names to have lowercase letters at beginning of property names.', () => {
   const jsonString = JSON.stringify({
     ValueA: 'some string',
     ValueB: 128
   } as ObjectInBackendFormat);
+
+  //Act
+  const parsedObject = parseToCamelCase(ObjectInFrontendFormat, jsonString);
+
+  //Assert
+  expect(parsedObject).toEqual({
+    valueA: 'some string',
+    valueB: 128
+  });
+});
+
+test('Expect deserialized JSON object to retain lowercase letters at beginning of property names.', () => {
+  const jsonString = JSON.stringify({
+    valueA: 'some string',
+    valueB: 128
+  } as ObjectInFrontendFormat);
 
   //Act
   const parsedObject = parseToCamelCase(ObjectInFrontendFormat, jsonString);
