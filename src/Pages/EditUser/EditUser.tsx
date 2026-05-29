8a990440410de9537140bd8713ffef4d252a6945
Benjamin Krug <benjaminkrug@yahoo.com>
2026-05-29 16:59:51 -0400

    Fix bugs in edit form

--- src/Pages/EditUser/EditUser.tsx
+++ src/Pages/EditUser/EditUser.tsx
@@ -1,5 +1,7 @@
 import { Effect } from 'effect/index';
 import { Form, Formik } from 'formik';
+import Switch from 'rc-switch';
+import 'rc-switch/assets/index.css';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import * as Yup from 'yup';
@@ -17,7 +19,7 @@ export default function EditUser() {
   let { userId } = useParams();
   let [ viewModel, setViewModel ] = useState(null as null | ViewUserModel);
   let [ editModel, setEditModel ] = useState(null as null | EditUserModel);
-  let [ roleList, setRoleList ] = useState([] as String[]);
+  let [ roleList, setRoleList ] = useState({} as { [key:string] : boolean });
   let [ hasBeenSaved, setHasBeenSaved ] = useState(false);
 
   //TODO: Maybe all of this data should just come from a single request
@@ -32,16 +34,23 @@ export default function EditUser() {
           onFailure: err => console.error(err)
         }))
       })
-      .then(() =>
-        getPagedData<String>('role')
-          .then(result =>
-            Effect.runPromise(Effect.match(result, {
-              onSuccess: (parsedResponse) => setRoleList(parsedResponse.data),
-              onFailure: (failureResponse) => console.log(failureResponse)
-            }))
-          )
-      )
   }, [userId]);
+  useEffect(() => {
+    getPagedData<String>('role')
+      .then(result =>
+        Effect.runPromise(Effect.match(result, {
+          onSuccess: (parsedResponse) => {
+            const userRoleNames = viewModel?.roles.map(r => r.name) ?? [];
+            setRoleList(Object.fromEntries(
+              parsedResponse.data
+                .map(role => role.toString())
+                .map(role => [role, userRoleNames.includes(role)])
+            ));
+          },
+          onFailure: (failureResponse) => console.log(failureResponse)
+        }))
+      )
+  }, [viewModel === null])
 
   let contactFieldForm = (
     editModel
@@ -78,19 +87,39 @@ export default function EditUser() {
       : <></>
   );
 
-  let roleForm =
-    roleList.length > 0
-    ? (<>
-      </>)
-    : (<></>);
+  let roleForm = (
+    Object.keys(roleList).length > 0
+    ? <Formik
+        initialValues={roleList}
+        enableReinitialize
+        onSubmit={(values, actions) => {
+          submitFormikJson<{ [key: string]: boolean }, {}>(`user/${userId}/roles`, values, actions, 'PUT')
+            .then(() => actions.setSubmitting(false));
+        }}>
+        {formik => (
+          <Form onSubmit={formik.handleSubmit}>
+            {Object.keys(formik.values).map(role => (
+              <div key={role}>
+                <label>{role}</label>
+                <Switch
+                  checked={formik.values[role]}
+                  onChange={(checked: boolean) => formik.setFieldValue(role, checked)}
+                />
+              </div>
+            ))}
+            <button type="submit" disabled={formik.isSubmitting}>Save Roles</button>
+          </Form>
+        )}
+      </Formik>
+    : <></>);
 
   return (
-    <>
+    <div className='form-parent'>
       {contactFieldForm}
       {roleForm}
       <div>
         Roles: {viewModel?.roles.map(r => r.name).join(', ')}
       </div>
-    </>
+    </div>
   )
 }
